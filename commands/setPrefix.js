2fb34ad62d1fe9233cf6c4cd8bc2135e3d419775
culinaryAmphibian <teperbudget@gmail.com>
2021-08-27 21:18:30 -0400

    fix error and oopsie

--- commands/setPrefix.js
+++ commands/setPrefix.js
@@ -1,14 +1,15 @@
 const { writeFileSync } = require('fs');
 const servers = require('../db/servers.json');
+const { ownerId } = require('../secret.json');
 
 module.exports = {
     name: 'setPrefix', description: 'allows admins to change my prefix in this server', usage: '<new prefix>setPrefix',
     execute(message) {
-        if (!message.author.hasPermission('ADMINISTRATOR')) return message.channel.send('you need to have admin perms to change the prefix');
+        if (!message.member.hasPermission('ADMINISTRATOR') && message.author.id != ownerId) return message.channel.send('you need to have admin perms to change the prefix');
         if (message.content == 'setPrefix') return message.channel.send('please specify a prefix');
         let newPrefix = message.content.match(/^(.)setPrefix$/);
         servers[message.guild.id].prefix = newPrefix[1];
         writeFileSync('./db/servers.json', JSON.stringify(servers, null, 2));
-        return message.channel.send(`success! my prefix in this server has been changed to \`${newPrefix}\``);
+        return message.channel.send(`success! my prefix in this server has been changed to \`${newPrefix[1]}\``);
     }
 }
